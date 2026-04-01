1c55f1f24d91bac0df8d43f4affd01d6cb040045
Morten Kjelsrud <mortkjels@gmail.com>
2026-04-01 22:16:31 +0200

    Got the visual of flask, removed example

--- front/logic.js
+++ front/logic.js
@@ -1,7 +1,5 @@
-async function testFlask() {
-    const response = await fetch("http://localhost:8250/");
-    const text = await response.text();
-    console.log(text);
+async function testPassword() {
+// her skal det være noe, finn ut. 
 }
 
-testFlask();
+testPassword();
